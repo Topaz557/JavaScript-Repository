9763d4bfca0e42a9cdead856bec04a0444f16246
Topaz557 <andreww9718@gmail.com>
2022-03-25 14:01:48 -0700

    update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,2 @@
+var Family= "The Webers", Dad = "Fred", Mom = "Chris", Daughter = "Laura", Son1 = "Jamie", Son2 = "Scott", Son3 = "Tyler", Son4 = "Charles", Son5 = "Andrew";
+document.write(3 + 3);
---
+++ Basic JavaScript Projects/Project1_expressions_alert/functionexample.js
@@ -0,0 +1,12 @@
+function My_First_Function() {
+    var str = "This is the Button text!";// Here we are assinging the var str to be display "this is the buton text"
+    document.getElementById("Button_Text").innerHTML = str; // By assinging the = str we are returning the string after the "button text" that we entered in html. This causes it to prompt to the new line of text once you have clicked the button
+}
+var A = "This is a string" //Here we are assinging text to Variable A
+var B = " combined with another"// Here we are assinging text to Variable B
+
+document.write (A + B.fontcolor("FF00")); // Here we are combining the two variables and displaying a color for the second.
+
+alert("Hello, I am an alert box!!")// Here we have created an alert box
+
+document.write (32 * 50) // Here we have created an expression where we times two numbers and write the results. 
