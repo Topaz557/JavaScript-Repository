976d7cc16f32a89f08c2f63422f84c9f4a2b4c92
Topaz557 <andreww9718@gmail.com>
2022-03-28 14:50:21 -0700

    update w comments

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,7 @@
-function myFunction(){
-   var f=document.getElementById("Paragraph");
-   f.innerText="here is the changed text";
-   console.log(f)
+function myFunction(){// Creating the function
+   var f=document.getElementById("Paragraph");// giving the function a value
+   f.innerText="here is the changed text";// giving the value to be returned by the funciton once we click
+   console.log(f) // This is a tool to look at the code when we are in inspection mode, it allows us to debug. 
    }
    
 
