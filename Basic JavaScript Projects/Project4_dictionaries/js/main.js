5c320f6391820da1a7dc881f7b8faf6289d8c433
Topaz557 <andreww9718@gmail.com>
2022-03-29 13:13:41 -0700

    Update main.js

--- Basic JavaScript Projects/Project4_dictionaries/js/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/js/main.js
@@ -1,11 +1,11 @@
-function my_Dictionary() {
-    var Animal = {
-        Species:"Fish",
-        Color:"Orange",
-        Breed:"ClownFish",
-        Age:"8",
-        Sound:"gurgle gurgle",
+function my_Dictionary() {// Here we create a dictionary
+    var Animal = { // Here we are creating a variable named Animals
+        Species:"Fish",// Here we are assining fish as the species
+        Color:"Orange",// Here we are assining orange as the color  
+        Breed:"ClownFish",// Here we are assining ClownFish as the breed
+        Age:"8",// Here we are assining the animals age
+        Sound:"gurgle gurgle",// Here we are assining the noise the animal makes
     };
-    delete Animal.Sound;
-    document.getElementById("Dictionary").innerHTML=Animal.Sound;
+    delete Animal.Sound; //Here we are deleting the sound the animal makes
+    document.getElementById("Dictionary").innerHTML=Animal.Sound; // Here we are asking for a return value of sound, however it has been removed so will display as undefined. 
 }
