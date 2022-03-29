8b79910166f6ea2ca029367e22849cf7589cc5a2
Topaz557 <andreww9718@gmail.com>
2022-03-29 12:55:26 -0700

    Update example.js

--- Basic JavaScript Projects/Project2_functions/JS/example.js
+++ Basic JavaScript Projects/Project2_functions/JS/example.js
@@ -1,7 +1,7 @@
-function myFunction(){
- var sentence =  "I am learning";
- sentence += " a lot from this book!";
- document.getElementById("Concatenate").innerHTML=sentence;
+function myFunction(){// here we are creating a function that can be called
+ var sentence =  "I am learning"; // Here we are creating a string
+ sentence += " a lot from this book!";// Here we are adding onto the string
+ document.getElementById("Concatenate").innerHTML=sentence;// Here we are calling the function to write the combined string
     }
     
  
