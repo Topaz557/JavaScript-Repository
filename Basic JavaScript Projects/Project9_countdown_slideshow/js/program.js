f6b1a3ad6e933680c18b7238b860f3b704b4d437
Topaz557 <andreww9718@gmail.com>
2022-04-06 13:45:05 -0700

    update

--- Basic JavaScript Projects/Project9_countdown_slideshow/js/program.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/js/program.js
@@ -1,13 +1,13 @@
-function countdown() {
-    var seconds = document.getElementById("seconds").value;
+function countdown() {// Here I am creating the countdown function
+    var seconds = document.getElementById("seconds").value;// Here I am creating the variables (seconds)
 
-    function tick() {
+    function tick() { // Here I am creating the "tick" function which will act as a countdown
         seconds = seconds - 1;
-        timer.innerHTML = seconds;
-        var time = setTimeout(tick, 1000);
-        if (seconds == -1) {
-            alert ("Time's up!");
-            clearTimeout(time);
+        timer.innerHTML = seconds; // Here I am setting the computers timer to seconds
+        var time = setTimeout(tick, 1000); // Here I am setting the value to 1000'ths of second
+        if (seconds == -1) { // Here I am creating a return value for when the seconds tick down and reach -1
+            alert ("Time's up!"); // Here I am creating an alert that will populate with the message times up when the countdown is reached
+            clearTimeout(time); // Here I am clearing the timer once the countdown has been reached so it can be reset and used to create another countdown based on value entered
             timer.innerHTML = "";
         }
     }
